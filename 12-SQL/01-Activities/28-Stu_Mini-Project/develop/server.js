7b7d6b4f73b4314096d301721066a6903e6b1b26
Morgan Henning <megannh6349@gmail.com>
2022-02-01 11:50:02 -0500

    a little help with the mini project ;)

---
+++ 12-SQL/01-Activities/28-Stu_Mini-Project/develop/server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: '',
+    // MySQL username,
+    user: '',
+    // TODO: Add MySQL password here
+    password: '',
+    database: ''
+  },
+  console.log(`Connected to the x database.`)
+);
+
+// Create a movie
+app.post('/api/new-movie', ({ body }, res) => {
+  const sql = `INSERT INTO movies (movie_name)
+    VALUES (?)`;
+  const params = [body.movie_name];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Read all movies
+app.get('/api/movies', (req, res) => {
+  // TODO
+  const sql = ``;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Delete a movie
+app.delete('/api/movie/:id', (req, res) => {
+  // TODO
+  const sql = ``;
+  const params = [];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+      message: 'Movie not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Read list of all reviews and associated movie name using LEFT JOIN
+app.get('/api/movie-reviews', (req, res) => {
+  // TODO
+});
+
+// BONUS: Update review name
+app.put('/api/review/:id', (req, res) => {
+  // TODO
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
